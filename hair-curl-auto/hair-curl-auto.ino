5f693e362cbb534f84554f5318941bd53d3ccc35
molecule <molecule@berkeley.edu>
2017-07-21 16:35:31 -0700

    Update for circuit testing - pin 3 goes high and pin 4 goes low.

--- hair-curl-auto/hair-curl-auto.ino
+++ hair-curl-auto/hair-curl-auto.ino
@@ -1,16 +1,26 @@
-const int hairPin = 3;
-const int ledPin = 13;
+//const int hairPin = 3;
+const int highPin = 3;
+const int lowPin = 4;
+//const int ledPin = 13;
 
 void setup() {
-  pinMode(hairPin, OUTPUT);
-  pinMode(ledPin, OUTPUT);
+  //pinMode(hairPin, OUTPUT);
+  //pinMode(ledPin, OUTPUT);
+  pinMode(highPin, OUTPUT);
+  pinMode(lowPin, OUTPUT);
 }
 
 void loop() {
+  digitalWrite(highPin, HIGH);
+  digitalWrite(lowPin, LOW);
+
+/*
+  
   digitalWrite(ledPin, HIGH);
   digitalWrite(hairPin, HIGH);
   delay(5000);
   digitalWrite(hairPin, LOW);
   digitalWrite(ledPin, LOW);
   delay(3000);
+  */
 }
